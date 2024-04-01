0be697aec50faf849cfc10fb63965ab7df5f870b
bugbat <85291787+bugbat@users.noreply.github.com>
2024-04-01 08:06:10 -0400

    move all messaging from console to dom

--- rpsgame.js
+++ rpsgame.js
@@ -1,87 +1,100 @@
 // global variables
-playerScore = 0;
-compScore = 0;
-ties = 0;
+let playerScore = 0;
+let compScore = 0;
+let ties = 0;
 
 // generate random integer between two values
 function getRandInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// generate a random move for the CPU player
-function getCompMove() {
-  let compMove = "";
-  let rInt = getRandInt(1, 3);
-  switch(rInt) {
-    case 1:
-      return compMove = "R";
-    case 2:
-      return compMove = "P";
-    case 3:
-      return compMove = "S";
-  }
-}
-
-// get player move
-function getPlayerMove() {
-  let playerMove = prompt("What is your move? Enter (R)ock, (P)aper, or (S)cissors ");
-  playerMove = playerMove.charAt(0).toUpperCase();
-  if (playerMove === "R" || playerMove === "P" || playerMove === "S") {
-    return playerMove;
-  }
-  else {
-    getPlayerMove();
-  }
-}
-// score the round
-function scoreRound(playerMove, compMove) {
-  if (playerMove === compMove) {
-    ties++
-    return "Tie";
-  }
-  else if (playerMove === "R") {
-    if (compMove === "P") {
-      compScore++
-      return "Computer wins!";
-    }
-    playerScore++
-    return "You win!";
-  }
-  else if (playerMove === "P") {
-    if (compMove === "S") {
-      compScore++
-      return "Computer wins!";
-    }
-    playerScore++
-    return "You win!";
-  }
-  else if (playerMove === "S") {
-    if (compMove === "R") {
-      compScore++
-      return "Computer wins!";
-    }
-    playerScore++
-    return "You win!";
-  }
-}
-
 // start gameplay
 function playGame(playerChoice) {
   // let roundCount = prompt("How many rounds would you like to play? ")
   for (i = 0; i < 1; i++) {
-    let playerMove = playerChoice;
-    let compMove = getCompMove();
-    console.log(`player played: ${playerMove} and computer played ${compMove}`);
-    console.log(scoreRound(playerMove, compMove))
+    let compChoice = getCompMove();
+    setChoices(playerChoice, compChoice);
+    scoreRound(playerChoice, compChoice);
   }
 }
 
-//playGame()
+// generate a random move for the CPU player
+function getCompMove() {
+  let rInt = getRandInt(1, 3);
+  switch(rInt) {
+    case 1:
+      return "Rock";
+    case 2:
+      return "Paper";
+    case 3:
+      return "Scissors";
+  }
+}
+
+// score the round
+function scoreRound(playerChoice, compChoice) {
+  if (playerChoice === compChoice) {
+    ties++;
+    setScores("ties", ties);
+    return;
+  }
+  else if (playerChoice === "Rock") {
+    if (compChoice === "Paper") {
+      compScore++;
+      setScores("compScore", compScore);
+      return;
+    }
+    playerScore++;
+    setScores("playerScore", playerScore);
+    return;
+  }
+  else if (playerChoice === "Paper") {
+    if (compChoice === "Scissors") {
+      compScore++;
+      setScores("compScore", compScore);
+      return;
+    }
+    playerScore++;
+    setScores("playerScore", playerScore);
+    return;
+  }
+  else if (playerChoice === "Scissors") {
+    if (compChoice === "Rock") {
+      compScore++;
+      setScores("compScore", compScore);
+      return;
+    }
+    playerScore++;
+    setScores("playerScore", playerScore);
+    return;
+  }
+}
+
+// update html display for choices
+function setChoices(playerChoice, compChoice) {
+  const playerChoiceSpan = document.querySelector("#playerchoice");
+  const compChoiceSpan = document.querySelector("#compchoice");
+  playerChoiceSpan.textContent = playerChoice;
+  compChoiceSpan.textContent = compChoice;
+}
+
+// update html display for scores
+function setScores(field, score) {
+  const scoreSpan = document.querySelector("#" + field);
+  scoreSpan.textContent = score;
+  if (score === 5 && field !== "ties") {
+    const winnerPar = document.querySelector("#winner");
+    if (field === "playerScore") {
+      winnerPar.textContent = "Player wins!"
+    }
+    else {
+      winnerPar.textContent = "Computer wins!"
+    }
+  }
+}
 
 // set up buttons to send choice to function
-
 const choiceButtons = document.querySelectorAll("button");
-
 
 choiceButtons.forEach(button => {
   button.addEventListener("click", function() {
